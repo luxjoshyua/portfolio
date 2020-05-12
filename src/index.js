15dd6a0f39f94f432d8601ecec19f1d3349ebe5a
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-05-12 11:52:59 +1000

    fixed console message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import style from "./sass/index.scss";
-// Add in wacky console message
 
 import "./js/cursor";
 import "./js/heading-colours";
@@ -9,6 +8,7 @@ import "./js/text-cycle";
 import "./js/drawings";
 import "./js/projects";
 
+// Add in wacky console message
 console.log(
   "%c This is the portfolio site of Joshua Fielding, a frontend developer, reach out at josh.e.fielding@gmail.com",
   "font-size: 50px;background:red;text-shadow:10px 10px 0 blue"
